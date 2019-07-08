f40bd1f59b67510832f8c48b9acc5151734a17ae
candra saputra <candrasaputra@live.com>
2019-07-08 15:19:47 +0700

    fix

--- phase-0-w3/1.js
+++ phase-0-w3/1.js
@@ -26,11 +26,11 @@
 2. Buat variabel `jenisKursi` untuk menampung jenis kursi yang dibeli peserta konser.
 3. Buat variabel `kembalian` untuk menampung kembalian dari pembelian tiket konser, isi kembalian dengan nominal 0 sebagai nilai awal.
 4. Buat variabel `uang` untuk menampung uang yang dimiliki peserta konser.
-5. Jika `uang` lebih besar dari 1000000, isi `jenisKursi` dengan VVIP, isi `kembalian` dengan hasil perhitungan `uang` dikurangi 1000000.
+5. Jika `uang` sama dengan atau lebih besar dari 1000000, isi `jenisKursi` dengan VVIP, isi `kembalian` dengan hasil perhitungan `uang` dikurangi 1000000.
 6. Kemudian tampilkan : `nama`-`jenisKursi` dan `kembalian`
-7. Lainnya jika `uang` lebih besar dari 700000, isi `jenisKursi` dengan VIP, isi `kembalian` dengan hasil perhitungan `uang` dikurangi 700000.
+7. Lainnya jika `uang` sama dengan atau lebih besar dari 700000, isi `jenisKursi` dengan VIP, isi `kembalian` dengan hasil perhitungan `uang` dikurangi 700000.
 8. Kemudian tampilkan : `nama`-`jenisKursi` dan `kembalian`
-9. Lainnya jika `uang` lebih besar dari 300000, isi `jenisKursi` dengan Reguler, isi `kembalian` dengan hasil perhitungan `uang` dikurangi 300000.
+9. Lainnya jika `uang` sama dengan atau lebih besar dari 300000, isi `jenisKursi` dengan Reguler, isi `kembalian` dengan hasil perhitungan `uang` dikurangi 300000.
 10. Kemudian tampilkan : `nama`-`jenisKursi` dan `kembalian`
 11. Lainnya peserta konser pulang.
 
