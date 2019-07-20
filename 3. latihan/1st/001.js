e84665001dc1ec7075464b19eec2afb91c72774e
candra saputra <candrasaputra@live.com>
2019-07-20 21:22:37 +0700

    1st

---
+++ 3. latihan/1st/001.js
@@ -0,0 +1,24 @@
+/**
+Buatlah sebuah algoritma untuk kasus berikut:
+Seorang pengajar sedang memeriksa ujian mahasiswa dan akan memberikan desc nilai dari A-E dengan ketentuan sebagai berikut:
+ - Nilai 80 - 100: A
+ - Nilai 65 - 79: B
+ - Nilai 50 - 64: C
+ - Nilai 35 - 49: D
+ - Nilai 0 - 34: E
+
+ Tampilkan desc nilai dan nama siswa saat pengajar tersebut memasukkan nilai dan nama yang dia inginkan.
+
+NOTED:
+Jika nilai mahasiswa kurang dari 0 atau lebih dari 100 maka tampilkan 'Nilai Invalid'
+**/
+
+/*
+1. Tampung nilai mahasiswa kedalam variabel `nilai`
+2. Periksa jika `nilai` lebih besar sama dengan 80 dan `nilai` kurang dan sama sedengan 100 tampung nilai sebagai A
+3. Periksa jika `nilai` lebih besar sama dengan 65 dan `nilai` kurang dan sama sedengan 79 tampung nilai sebagai B
+4. Periksa jika `nilai` lebih besar sama dengan 50 dan `nilai` kurang dan sama sedengan 64 tampung nilai sebagai C
+5. Periksa jika `nilai` lebih besar sama dengan 35 dan `nilai` kurang dan sama sedengan 49 tampung nilai sebagai D
+6. Periksa jika `nilai` lebih besar sama dengan 0 dan `nilai` kurang dan sama sedengan 34 tampung nilai sebagai E
+7. Urutkan nilai berdasarkan A ke E.
+*/
